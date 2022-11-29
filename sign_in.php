a1c4f94c648d5b7671f4797ec359e893147195d2
medabra <abrache2001@gmail.com>
2022-11-29 09:26:24 +0100

    adding sign In and register part

--- sign_in.php
+++ sign_in.php
@@ -17,7 +17,56 @@ include 'scripts.php';
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"/>
     <!-- ================== END core-css ================== -->
   </head>
-<body">
+
+<body style="background-color: #eee;">
+   
+   <section >
+       <div class="container vh-100">
+         <div class="row d-flex justify-content-center align-items-center h-100">
+           <div class="col-lg-12 col-xl-11">
+             <div class="card text-black" style=" border-radius: 10px;">
+               <div class="card-body p-md-5">
+                 <div class="row justify-content-center">
+                   <div class=" col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+     
+                     <h1 class="text-center fw-bold mb-4 mx-1 mx-md-4 mt-4">Welcome Back!</h1>
+                     <p class="text-center h6 text-muted mb-5 mx-1 mx-md-4 mt-4"> Login with your details to continue </p>
+
+                   
+                     <form  method="post"  data-parsley-validate >
+
+                <!-- To show errors is user put wrong data -->
+
+                 <div class=" mb-4">
+                 <i class="fas fa-envelope fa me-2 fa-fw pt-4"></i>
+                 <label class="form-label fw-bold" for="Logemail ">Email address :</label>
+                   <input type="email" id="Logemail" name="email" class="form-control form-control"  />
+                 </div>
+     
+                 <div class=" mb-4">
+                 <i class="fas fa-lock fa me-2 fa-fw pt-4"></i>
+                 <label class="form-label fw-bold" for="lpassword">Password :</label>
+                   <input type="password" id="lpassword" name="password" class="form-control form-control"  />
+                   
+                 </div>
+
+                 <div class="pt-1 mb-4">
+                   <button class="btn btn-primary btn btn-block container fluid " type="submit"  name="logIn" >Login</button>
+                 </div>
+     
+                 <p class="text-center">Don't have an account? <a href="" class="fw-bold text-black  text-decoration-none">Sign Up</a></p>
+     
+               </form>
+     
+                   </div>
+                  
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+       </div>
+     </section>
 </body>
     <!-- ================== BEGIN core-js ================== -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
--- sign_up.php
+++ sign_up.php
@@ -9,7 +9,7 @@ include 'scripts.php';
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Sign in</title>
+        <title>Register</title>
     <!-- ================== BEGIN core-css ================== -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link href="assets/css/default/app.min.css" rel="stylesheet" />
@@ -17,7 +17,84 @@ include 'scripts.php';
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"/>
     <!-- ================== END core-css ================== -->
   </head>
-<body>
+
+<body style="background-color: #eee;">
+   
+   <section >
+       <div class="container vh-100">
+         <div class="row d-flex justify-content-center align-items-center h-100">
+           <div class="col-lg-12 col-xl-11">
+             <div class="card text-black" style=" border-radius: 10px;">
+               <div class="card-body p-md-5">
+                 <div class="row justify-content-center">
+                   <div class=" col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+     
+                     <h1 class="text-center fw-bold mb-4 mx-1 mx-md-4 mt-4">Let's Get Started</h1>
+                     <p class="text-center h6 text-muted mb-5 mx-1 mx-md-4 mt-4"> Add your personal details to continue </p>
+
+                   <form class="mx-1 mx-md-4" action="" method="post" >
+      
+                             <!-- 2 column grid layout with text inputs for the first and last names -->
+                                <div class="row">
+
+                                <div class="col-md-6 d-flex flex-row align-items-center mb-4">
+                                
+                                <div class="flex-fill mb-0">
+                                    <i class="fas fa-user fa me-3 fa-fw pt-4 " ></i>
+                                        <label class="form-label fw-bold" for="fname">First name:</label>
+                                        <input type="text" name="Firstname"  id="fname" class="form-control" /> 
+                                    </div>
+
+                                    </div>
+                                    <div class="col-md-6 d-flex flex-row align-items-center mb-4">
+                                        
+                                        <div class="flex-fill mb-0">
+                                            <i class="fas fa-user fa me-3 fa-fw pt-4 "></i>
+                                          <label class="form-label fw-bold" for="lname">Last name:</label>
+                                        <input type="text" name="Lastname" id="lname" class="form-control" />
+                                        
+                                    </div>
+
+                                    </div>
+                                </div>
+                                <div class=" mb-4">
+                                    <i class="fas fa-home fa me-2 fa-fw pt-4"></i>
+                                    <label class="form-label fw-bold" for="address ">Address :</label>
+                                      <input type="text" id="address" name="address" class="form-control form-control"/>
+                                    </div>
+
+                                    <div class=" mb-4">
+                                        <i class="fas fa-envelope fa me-2 fa-fw pt-4"></i>
+                                        <label class="form-label fw-bold" for="CIN ">CIN :</label>
+                                          <input type="CIN" id="CIN" name="CIN" class="form-control form-control"  />
+                                        </div>
+                        
+                                    <div class=" mb-4">
+                                    <i class="fas fa-calendar fa me-2 fa-fw pt-4"></i>
+                                    <label class="form-label fw-bold" for="date">date of Birth :</label>
+                                      <input type="date" id="lpassword" name="date" class="form-control form-control" />
+                                      
+                                    </div>
+                   
+                                    <div class="pt-1 mb-4 row justify-content-between">
+                                      <button class="btn btn-primary btn btn-block col-5 " type="submit"  name="register" >Register</button>
+                                      <button class="btn btn-primary btn btn-block col-5 " type="submit"  name="register" >Next</button>
+                                    </div>
+                        
+                                    <p class="text-center">Already have an account? <a href="" class="fw-bold text-black  text-decoration-none">Log in</a></p>
+
+          
+                          </form>
+     
+                   </div>
+                  
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+       </div>
+     </section>   
 </body>
     <!-- ================== BEGIN core-js ================== -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
