e0031307173c9e5b69f61412873512c9b5465d21
ABDELMAJID ELOUSSE <mjid.elouss@gmail.com>
2022-11-29 14:44:53 +0100

    dashboard and dash style

--- Patient/dashboard.php
+++ Patient/dashboard.php
@@ -1,6 +1,6 @@
-<?php
+<!-- <?php
 include 'scripts.php';
-?>
+?> -->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -10,7 +10,7 @@ include 'scripts.php';
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Admin | Dashboard</title>
+    <title>Dashboard</title>
     <!-- ================== BEGIN core-css ================== -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
@@ -18,10 +18,23 @@ include 'scripts.php';
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
         integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
+    <link rel="stylesheet" href="./style.css"/>
     <!-- ================== END core-css ================== -->
 </head>
 
 <body>
+<div class="d-flex" id="wrapper">
+        <!-- Sidebar -->
+        <div class="bg-white" id="sidebar-wrapper">
+            <div class="sidebar-heading ms-2 py-4 primary-text fs-4 fw-bold text-uppercase border-bottom"><i
+                    class=""></i>Account
+            </div>
+            <div class="list-group list-group-flush my-3">
+                <a href="#" class="list-group-item list-group-item-action bg-transparent second-text"><i
+                        class="fas fa-tachometer-alt me-2"></i>Dashboard</a>
+            </div>
+        </div>
+        <!-- Page Content -->
 </body>
 <!-- ================== BEGIN core-js ================== -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
