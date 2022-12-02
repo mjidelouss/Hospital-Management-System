5d3f332e3b7e16943f3c0250ea1530249567578b
AZdevloper <ez.zahed.abdelaziz@gmail.com>
2022-12-02 23:44:13 +0100

    add nav_bar

---
+++ Admin/Schedule.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Dashboard</title>
+    <!-- ================== BEGIN core-css ================== -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="../assets/css/default/app.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+        integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
+    <link rel="stylesheet" href="./style2.css"/>
+    <!-- ================== END core-css ================== -->
+</head>
+<body>
+<div class="d-flex" id="wrapper">
+        <!-- Sidebar -->
+        <div id="sidebar-wrapper">
+                    <div class="d-flex pt-3">
+                        <img src="../assets/img/user.png" class="rounded-circle ms-4" width="70" alt="Image Not Found">
+                        <div class="ms-3 mt-2">
+                            <h5>Admin</h5>
+                            <h6 class="user_email">admin@gmail.com</h6>
+                        </div>
+                    </div>
+                    <div class="mt-3 ms-4"><a href="#" class="btn bg-info px-5 bg-opacity-25 w-75 fw-bold" style="color: rgb(73, 166, 243);">log out</a></div>
+                    <hr>
+                        <div class="list-group-flush ms-3 list-group">
+                            <a href="admin.html" class="list-group-item bg-transparent"><img src="../assets/img/icons/dashboard.svg" alt=""> Dashboard</a>
+                            <a href="#" class="list-group-item bg-transparent"><img src="../assets/img/icons/doctors.svg" alt=""> Doctors</a>
+                            <a href="#" class="list-group-item bg-transparent"><img src="../assets/img/icons/schedule.svg" alt=""> Schedule</a>
+                            <a href="#" class="list-group-item bg-transparent"><img src="../assets/img/icons/book.svg" alt=""> Appointment</a>
+                            <a href="#" class="list-group-item bg-transparent"><img src="../assets/img/icons/patients.svg" alt=""> Patient</a>
+                        </div>
+                </div>
+            <!-- Page Content -->
+        <div id="page-content-wrapper">
+            <nav class="navbar navbar-expand-lg bg-transparent py-4 px-4 d-flex justify-content-between">
+                <div class=" d-flex ">
+    
+                    <div class="d-flex align-items-center">
+                        <i class="fas fa-bars primary-text fs-4 me-3" style="color: black; cursor: pointer;"
+                            id="controlPanel" onclick="wrapside()"></i>
+                    </div>
+                    <div class=" row bg-blue-100  rounded-1  p-2" >
+                        <img src="../assets/icon/back-iceblue.svg" alt="" class="  col">
+                        <p class=" col align-items-center m-0 fw-800 fs-5 "> Back</p>
+    
+                    </div>
+    
+                   
+                    
+                </div> 
+                <div class=" p-1 ms-4 fs-4 fw-800  me-auto" >
+                        
+                      <p class="my-0">Shedule Manager</p>  
+                        
+                    </div>
+
+
+                <div class=" d-flex" >
+
+                    <div class="row flex-column " dir="rtl" >
+                        <p class=" col m-0 fs-6 fw-600" > Today'y Date</p>
+                        <p class=" col m-0 fs-5 fw-800 " > 2022-11-01</p>
+
+                         </div  >
+                             <div class=" p-2 bg-light-600 rounded-1 ms-2 " >
+
+                                 <img src=" ../assets/icon/calendar.svg" >
+                        
+                             </div>
+
+                </div>
+            </nav>
+            <h5 class="fw-bold ms-4 d-inline-block">Schedule a Session</h5> <a class=" list-group-item  p-2 rounded-1 shadow  d-inline-block bg-blue-500 fs-5 fw-800 text-light "> <img src=" ../assets/icon/add.svg" > Add a Session </a>
+            <div class="container-fluid px-4">
+                <h4> All Session </h4>
+                <div class=" d-flex p-2 border rounded justify-content-around">
+                    <div>
+                         <label for="date"> Date</label>
+                        <input type="date" name="" id="" class=" rounded-1 " style=" width: 15rem;" >
+                    </div>
+
+                    <div>
+                        
+                        <label for="Date"> Doctor :</label>
+
+                            <select id="Date">
+                            <option value=""> Choose Doctor Name from the list</option>
+                            
+                            </select>
+                    </div>
+                    <div class=" d-flex">
+                        <img src="../assets/icon/filter-iceblue.svg" alt="" srcset="">
+                        <p>Filter</p>
+                    </div>
+                    
+                   
+                </div>
+                
+            </div>
+        </div>
+</div>
+<!-- ================== BEGIN core-js ================== -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+</script>
+<script src="./scripts.js"></script>
+<!-- ================== END core-js ================== -->
+</html>
