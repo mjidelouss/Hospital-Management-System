2834a3f767d5cc8fdd2417474811886ffecf8e68
ABDELMAJID ELOUSSE <77199542+mjidelouss@users.noreply.github.com>
2022-12-04 13:54:05 +0100

    Update DbConnection.php
    class database

--- classes/DbConnection.php
+++ classes/DbConnection.php
@@ -1,5 +1,17 @@
 <?php 
 
-/*  your connection class goes here 
-    hint : use PDO . 
-*/
+class DbConnection {
+    private $host = "localhost";
+    private $user = "root";
+    private $pwd = "";
+    private $dbName = "youcodehospital";
+
+    protected function connect() {
+        $dsn = 'mysql:host='.$this->host.';dbname='.$this->dbName;
+        $pdo = new PDO($dsn, $this->user, $this->pwd);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $pdo;
+    }
+}
+$sql = "";
+$sql->connect()->$pdo;
