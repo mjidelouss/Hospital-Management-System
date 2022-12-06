eec5641091958be277f69fea1b632e8e289b0dc7
ABDELMAJID ELOUSSE <mjid.elouss@gmail.com>
2022-12-06 09:22:29 +0100

    modified some classes

--- classes/doctor.class.php
+++ classes/doctor.class.php
@@ -1,7 +1,7 @@
 <?php
-include "autoloader.php";
+require_once('../includes/autoloader.php');
 
-class User {
+class Doctor extends DbConnection{
     public $id;
     public $firstName;
     public $lastName;
@@ -12,6 +12,22 @@ class User {
 
     }
     public function displayAppointment() {
-        
+        $sql = "SELECT * FROM appointment";
+        $stmt = $this->connect()->query($sql);
+        while ($row = $stmt->fetch()) 
+        {
+          echo '
+          <tr>
+          <th scope="row">' . $row["First_name"] . ' ' . $row["Last_name"] . '</th>
+          <td class="fs-2 text-primary text-center">2</td>
+          <td>Test Session</td>
+          <td>2050-01-01 @18:00</td>
+          <td>2022-12-31</td>
+          <td class="d-flex justify-content-evenly">
+            <button type="submit" class="btn bg-info fw-bold rounded bg-opacity-25" style="color: #03639f;"><img class="me-2 mb-1" src="../assets/img/icons/delete-iceblue.svg">Cancel</button>
+        </td>
+          </tr>
+          ';
+        }
     }
 }
