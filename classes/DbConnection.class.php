6d6544c9f9329dfb3ae58ee05d2dfcc3e5eff820
ABDELMAJID ELOUSSE <mjid.elouss@gmail.com>
2022-12-09 09:45:37 +0100

    oop backend

--- classes/DbConnection.class.php
+++ classes/DbConnection.class.php
@@ -1,15 +1,20 @@
 <?php 
 
 class DbConnection {
-    private $host = "localhost";
-    private $user = "root";
-    private $pwd = "";
-    private $dbName = "youcodehospital";
+    public $host = "localhost";
+    public $user = "root";
+    public $pwd = "";
+    public $dbName = "youcodehospital";
 
-    protected function connect() {
-        $dsn = 'mysql:host='.$this->host.';dbname='.$this->dbName;
-        $pdo = new PDO($dsn, $this->user, $this->pwd);
-        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        return $pdo;
-    }
+    public function connect() {
+        try{
+            $dsn = 'mysql:host='.$this->host.';dbname='.$this->dbName;
+            $pdo = new PDO($dsn, $this->user, $this->pwd);
+            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            return $pdo;
+         }
+         catch(PDOException $e){
+             echo "Error: " . $e->getMessage();
+           }
+     }
 }
