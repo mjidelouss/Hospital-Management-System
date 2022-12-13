b2fb7b912881f5d40e7580d6a4bf8cd071091e78
AZdevloper <ez.zahed.abdelaziz@gmail.com>
2022-12-13 22:06:50 +0100

    update user

--- classes/user.class.php
+++ classes/user.class.php
@@ -1,5 +1,4 @@
 <?php
-require_once('../includes/autoloader.php');
 
 
 class User extends DbConnection
@@ -9,37 +8,40 @@ class User extends DbConnection
     public $lastName;
     public $email;
     public $password;
+    public $db;
 
-
-
+    public function __construct() {
+      $this->db = new DbConnection;
+    }
+    
     public function login($email, $password)
     {
-
-
-
-
             if (!empty($email) && !empty($password)) {
 
-                $query = $this->pdo->prepare("SELECT * FROM `role` WHERE email = ? limit 1");
+                $query = $this->connect()->prepare("SELECT * FROM `role` WHERE email = ? limit 1");
                 $query->execute(array($email));
                 $role = $query->fetch();
 
                
             if($role){
 
-                if ($role["rol_type"] == "patient") {
-                    $query = $this->pdo->prepare("SELECT * FROM `patient` WHERE email = ? AND password = ? limit 1");
+                if ($role["role"] == "patient") {
+                    $query = $this->connect()->prepare("SELECT * FROM `patient` WHERE email = ? AND password = ? limit 1");
                     $query->execute(array($email, $password));
                     $role = $query->fetchAll();
 
                     $_SESSION['user'] = $role;
+                    echo "<pre>";
+                    // var_dump($_SESSION['user'][0]["id"]);
+                    echo "</pre>";
+                    // die();
                     $_SESSION['conn'] = 'success';
                     header('Location:Patient/dashboardPatient.php');
 
 
-                } elseif ($role["rol_type"] == "doctor") {
+                } elseif ($role["role"] == "doctor") {
 
-                    $query = $this->pdo->prepare("SELECT * FROM `doctor` WHERE email = ? AND password = ? limit 1 ");
+                    $query = $this->connect()->prepare("SELECT * FROM `doctor` WHERE email = ? AND password = ? limit 1 ");
                     $query->execute(array($email, $password));
                     $role = $query->fetchAll();
 
@@ -47,9 +49,9 @@ class User extends DbConnection
                     $_SESSION['conn'] = 'success';
                     header('Location:Doctor/dashboardDoctor.php');
 
-                } elseif ($role["rol_type"] == "admin") {
+                } elseif ($role["role"] == "admin") {
 
-                    $query = $this->pdo->prepare("SELECT * FROM `doctor` WHERE email = ? AND password = ? limit 1");
+                    $query = $this->connect()->prepare("SELECT * FROM `admin` WHERE email = ? AND password = ? limit 1");
                     $query->execute(array($email, $password));
                     $role = $query->fetchAll();
 
@@ -64,79 +66,23 @@ class User extends DbConnection
 
         }
         }
-        //
-        //
-        //        echo "<pre>"."patient  ";
-        //        var_dump($patient);
-        //        echo "</pre>";
-        //
-        //        if ($admin) {
-        //            $_SESSION['user'] = $admin;
-        //
-        //            $_SESSION['conn'] = 'success' ;
-        //
-        //
-        //                header('Location:Admin/dashboardAdmin.php');
-        //
-        //
-        //        }elseif($doctor){
-        //
-        //            $_SESSION['user'] = $doctor;
-        //            $_SESSION['conn'] = 'success' ;
-        //            header('Location:Doctor/dashboardDoctor.php');
-        //        }elseif ($patient) {
-        //
-        //            $_SESSION['user'] = $patient;
-        //            $_SESSION['conn'] = 'success' ;
-        //            header('Location:Patient/dashboardPatient.php');
-        //
-        //
-        //        }else {
-        //
-        //            $_SESSION['login_error'] = "invalid email or password !";
-        //        }
-        //
-
-
-
-        // if ($result_admin) {
-
-        //     header('Location:Admin/dashboardAdmin.php');
-
-
-        // }else {
-        //  echo "no accoount ";
-        // }
-
-        // $is=0;
-        // foreach( $result as $row ) {
-
-        //         if($email==$row['email']){
-        //                 $is++;
-
-        //         }
-
-        // }
-        //         if ($is>0) {
-        //             $is++;
-        //             $_SESSION["userfrnam"] = $row["first_name"];
-        //             $_SESSION["userlsnam"] = $row["last_name"];
-        //             $_SESSION["useremail"] = $row["email"];
-        //             $_SESSION["userid"] = $row["id"]	;
-
-        //             $_SESSION['con'] = 'success' ;
-        //             header("Location: index.php");
-        //         }else {
-
-        //             $_SESSION['login_error'] = 'email or password is incorrect ' ;
-        //             header("Location: sign_in.php");
-
-        //         }
 
     }
 
 
     public function displayAppointment()
     {
+    }
+    
+    public function get_all($user){
+  
+        $db = new DbConnection();
+        
+        $sql = "SELECT * FROM `$user`";
+        $STH = $db->connect()->prepare($sql);
+        $STH->execute();
+        $res = $STH->rowCount();
+
+        return $res;
     }
 }
